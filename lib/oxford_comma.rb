f1bd88a9e10a742679e2dc7dda5110f9f2f7f99a
Alli Tzadok <allisontzadok@gmail.com>
2018-01-06 22:46:52 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
-if array.nil?
-  return nil
-if array.length == 1
-  return array[0]
-if array.length > 1
-return array[0..-2].join(", ")+" and "+array[-1]
-end
+  if array.nil?
+    return nil
+    if array.length == 1
+      return array[0]
+      if array.length > 1
+        return array[0..-2].join(", ")+" and "+array[-1]
+      end
