0bfb22e9117d23cc8f5e7879c31747feb2814bca
Alli Tzadok <allisontzadok@gmail.com>
2018-01-06 23:25:58 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     return array[0]
   elsif array.length == 2
     return "#{array[0]} and #{array[1]}"
-  else
+  else array.length > 2
     return array[0..-2].join(', ') + " and " + array[-1]
   end
 end
