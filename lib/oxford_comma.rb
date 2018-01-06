486c4fb9c64cf6ac7ae229657fd2db3585898a07
Alli Tzadok <allisontzadok@gmail.com>
2018-01-06 22:45:39 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-  return array.to_a_sentence
+if array.nil?
+  return nil
+if array.length == 1
+  return array[0]
+if array.length > 1
+return array[0..-2].join(", ")+" and "+array[-1]
 end
